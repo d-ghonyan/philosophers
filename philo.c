4685a7a302a0744377513ef68bb57503638c077c
Davit Ghonyan <dghonyan@c2r4s4.42yerevan.am>
2022-06-23 17:50:19 +0400

    aaaa

--- philo.c
+++ philo.c
@@ -2,30 +2,43 @@
 
 void	*start_routine(void *arg)
 {
-	printf("IM THREAD NUMBER %d", *(int *)arg);
+
+	//while ()
+	printf("%p\n", &(((t_thread_info *)(arg))->mutex));
+	// printf("IM THREAD NUMBER %d", ((t_thread_info *)arg)->id);
 	return (NULL);
+}
+
+void	init_thread()
+{
+
 }
 
 int	main(int argc, char **argv)
 {
+	int				size;
+	int				i;
+	t_thread_info	*threads;
+
 	if (check_args(argc, argv) < 0)
 		return (1);
-	// int			size;
-	// int			i;
-	// t_thread_info	*threads;
-	// //pthread_t	*threads;
+	i = 0;
+	size = ft_atoi(argv[1]);
+	threads = malloc(sizeof (*threads) * size);
 
-	// i = 0;
-	// size = ft_atoi(argv[1]);
-	// threads = malloc(sizeof (*threads) * size);
-	// while (i < size)
-	// {
-	// 	pthread_create(&(threads[i].id), NULL, &start_routine, &(threads[i].id));
-	// 	i++;
-	// }
-	// //pthread_create(&thread, NULL, &start_routine, "hello");
-	// //pthread_create(&thread, NULL, &start_routine, "hello");
-	// for (int i = 0; i < size; i++)
-	// 	pthread_join(threads[i].id, NULL);
+	i = 0;
+	while (i < size)
+	{
+		//threads
+		pthread_mutex_init(&(threads[i].mutex), NULL);
+		pthread_create(&(threads[i].id), NULL, &start_routine, &(threads[i]));
+		i++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		pthread_join((threads[i].id), NULL);
+		i++;
+	}
 	// printf("MAIN RETURNED");
 }
--- philo.h
+++ philo.h
@@ -11,8 +11,14 @@
 # include "colors.h"
 
 typedef struct	s_thread_info {
-	pthread_t	id;
-	char		*msg;
+	int				num;
+	int				to_eat;
+	int				to_die;
+	int				to_sleep;
+	int				must_eat;
+	time_t			time;
+	pthread_t		id;
+	pthread_mutex_t	mutex;
 } t_thread_info;
 
 int	ft_atoi(char *s);
